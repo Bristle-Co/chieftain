fab7e82eb9d8d0db8318ff4e1e7d5ead556f974b
Anderson Hsieh <andy13579246810@gmail.com>
2022-07-10 18:43:04 -0400

    setup basic getServerSideProps for customer, also defined base api url in next.config.js

--- pages/customer_detail/index.js
+++ pages/customer_detail/index.js
@@ -1,6 +1,7 @@
 import { TopBarStateContext } from "../../components/context.js";
 import { useContext } from "react";
 import { IoAdd } from "react-icons/io5";
+import axios from "axios";
 import Link from "next/link";
 
 const customerDetailTopBarState = {
@@ -18,16 +19,40 @@ const customerDetailTopBarState = {
   ],
 };
 
-const CustomerDetail = () => {
+export async function getServerSideProps(context) {
+  let customers;
+  try {
+    const result = await axios({
+      method: "get",
+      url: "/customer_detail/getCustomers",
+      baseURL: process.env.backendServerBaseURI,
+      params: {
+        pageIndex: 0,
+        pageSize: 20,
+      },
+    });
+    customers = result.data.data;
+  } catch (error) {}
+  console.log("actual data");
+  console.log(customers);
+
+  return {
+    props: {
+      data: customers,
+    },
+  };
+}
+
+const CustomerDetail = (props) => {
   const [topBarState, setTopBarState] = useContext(TopBarStateContext);
   setTopBarState({});
   setTopBarState(customerDetailTopBarState);
 
   return (
-    <h3>
-      customer
-      detaileeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-    </h3>
+    <>
+      <h3>customer</h3>
+      <p>{JSON.stringify(props.data[1])}</p>
+    </>
   );
 };
 
